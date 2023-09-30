fb53fdf0b2eae5dacb277dc39d3ce35c322ef4fe
PODA <v016604@gmail.com>
2023-09-30 23:07:36 +0800

    新增setting.json檔:fix：尋找不到setting的位子

--- Discord_BasicBot.py
+++ Discord_BasicBot.py
@@ -1,5 +1,9 @@
 import discord
 from discord.ext import commands
+import json
+
+with open("D:\work_space\Discord_BasicBot\setting.json",'r',encoding='utf-8') as jfile:
+    jdata=json.load(jfile)
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -8,22 +12,22 @@ intents.members = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
-async def on_ready():
+async def on_ready():                               #bot啟動訊息
     print(f'We have logged in as {bot.user}')
 
 @bot.event                                          #成員進入
 async def on_member_join(member):
-    chanel=bot.get_channel(1157653005324255296)
+    chanel=bot.get_channel(int(jdata["Channel"]))
     await chanel.send(F'{member}join!')
     
 
 @bot.event
 async def on_member_remove(member):                 #成員離開
-    chanel=bot.get_channel(1157653005324255296)
+    chanel=bot.get_channel(int(jdata["Channel"]))
     await chanel.send(F'{member}leave!')
 
-@bot.command()
+@bot.command()                                      #ping顯示
 async def ping(ctx):
     await ctx.send(F'{round(bot.latency*1000)}(ms)')
 
-bot.run('MTE1NzY0OTg0Nzg4NTc3MDg4Mw.GXiMPf.nfS20TtOvcMXItrNLwI5nFugtH5NuroBDUt2F4')
+bot.run(jdata["TOKEN"])                             #bot執行

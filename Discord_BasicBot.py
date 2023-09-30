ec1763dc241aba57d47f70a0abe3c91ecf48a532
PODA <v016604@gmail.com>
2023-09-30 23:41:38 +0800

    新增本地與網路照片顯示功能

--- Discord_BasicBot.py
+++ Discord_BasicBot.py
@@ -1,6 +1,10 @@
-import discord
 from discord.ext import commands
+import discord
+import asyncio
 import json
+import random
+import aiohttp
+import io
 
 with open("D:\work_space\Discord_BasicBot\setting.json",'r',encoding='utf-8') as jfile:
     jdata=json.load(jfile)
@@ -15,11 +19,10 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 async def on_ready():                               #bot啟動訊息
     print(f'We have logged in as {bot.user}')
 
-@bot.event                                          #成員進入
-async def on_member_join(member):
+@bot.event                                          
+async def on_member_join(member):                   #成員進入
     chanel=bot.get_channel(int(jdata["Channel"]))
     await chanel.send(F'{member}join!')
-    
 
 @bot.event
 async def on_member_remove(member):                 #成員離開
@@ -29,5 +32,14 @@ async def on_member_remove(member):                 #成員離開
 @bot.command()                                      #ping顯示
 async def ping(ctx):
     await ctx.send(F'{round(bot.latency*1000)}(ms)')
+
+@bot.command()                                      #隨機本機照片
+async def randomPicture(ctx):
+    pic=discord.File(random.choice(jdata['pic']))
+    await ctx.send(file=pic)
+
+@bot.command()                                      #隨機網路照片
+async def webPicture(ctx):
+    await ctx.send(random.choice(jdata['url_pic']))
 
 bot.run(jdata["TOKEN"])                             #bot執行

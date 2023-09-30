540846a427a44f3d927567413936362e09bf8d7c
PODA <v016604@gmail.com>
2023-09-30 21:29:53 +0800

    新增member進入離開

--- Discord_BasicBot.py
+++ Discord_BasicBot.py
@@ -17,4 +17,15 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
+@client.event
+async def on_member_join(member):
+    chanel=client.get_channel(1157653005324255296)
+    await chanel.send(F'{member}join!')
+    
+
+@client.event
+async def on_member_remove(member):
+    chanel=client.get_channel(1157653005324255296)
+    await chanel.send(F'{member}leave!')
+
 client.run('MTE1NzY0OTg0Nzg4NTc3MDg4Mw.GXiMPf.nfS20TtOvcMXItrNLwI5nFugtH5NuroBDUt2F4')
